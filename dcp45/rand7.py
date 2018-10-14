be390e6b2ef7c89a864154522f4fecb195ce0644
Stephen C <cranegames@gmail.com>
2018-10-14 13:42:34 -0700

    Complete rand7

--- dcp45/rand7.py
+++ dcp45/rand7.py
@@ -4,25 +4,30 @@ class RNG:
     def rand5(self):
         return (randrange(1,6))
     def rand7(self):
-        x = self.rand5()
-        assert(x<=5 and x>=1)
-        y = self.rand5() - 1
-        while(y >= 3):
-            y = self.rand5() - 1
-        assert(y<=2 and y>=0)
-        return x + y
+        def l3():
+            x = self.rand5() - 1
+            while(x > 2):
+                x = self.rand5() - 1
+            return x
+        x,y = l3(), l3()
+        z = x + y * 3 + 1
+        while(z > 7):
+            x,y = l3(), l3()
+            z = x + y * 3 + 1
+        return z
     def sim_dist(self):
         f = self.rand7
-        rand_arr = [0]*9
-        total_runs = 10000
+        rand_arr = [0]*7
+        total_runs = 100000
         for _ in range(0,total_runs):
-            rand_arr[f()] = rand_arr[f()] + 1
+            i = f() -1
+            rand_arr[i] = rand_arr[i] + 1
         totes = 0
         for x in rand_arr:
             totes += x
         print(f'Totes: {totes}')
-        for i in range(1,8):
-            print(f'{i} occurences: {rand_arr[i]}  %: {rand_arr[i]/totes}')
+        for i, n in enumerate(rand_arr):
+            print(f'{i+1} occurences: {n:>5}  : {n/totes*100:>5.4}% +- {abs(1/7 - n/totes)*100:.5}')
 
 
 if __name__ == "__main__":
